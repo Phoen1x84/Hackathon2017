10caed9a80eb5dfade4f6b283e4e1b139b62034b
islaytitans <jonathan.m.robbins@gmail.com>
2017-03-04 17:08:39 +0000

    Dev / initial work on the controller to save hotspot

---
+++ AfternoonDelight/AfternoonDelight.Feature.Media/Models/HotspotCoordinateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AfternoonDelight.Feature.Media.Models
+{
+    public class HotspotCoordinateViewModel
+    {
+        public string HostspotImageId { get; set; }
+        public int XLocation { get; set; }
+        public int YLocation { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
--- AfternoonDelight/AfternoonDelight.Feature.Media/Repositories/IHotspotImageRepository.cs
+++ AfternoonDelight/AfternoonDelight.Feature.Media/Repositories/IHotspotImageRepository.cs
@@ -1,9 +1,11 @@
+using AfternoonDelight.Feature.Media.Models;
+using Sitecore.Data;
 using Sitecore.XA.Foundation.Mvc.Repositories.Base;
 
 namespace AfternoonDelight.Feature.Media.Repositories
 {
     public interface IHotspotImageRepository : IModelRepository, IAbstractRepository<IRenderingModelBase>
     {
-
+        void CreateHotspot(ID hotspotImageId, HotspotModel hotspotModel, string DatabaseName);
     }
 }
